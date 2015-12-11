1732ccfd3dbfe3b0ef234900791a20723b0f22fc
E. Kevin Hall <hall@absinthe.org>
2015-12-11 16:16:49 -0500

    first commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject mqtt "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clojurewerkz/machine_head "1.0.0-beta8"]
+                 [compojure "1.1.8"]
+                 [ring/ring-jetty-adapter "1.2.2"]
+                 [environ "0.5.0"]])
---
+++ src/mqtt/core.clj
@@ -0,0 +1,27 @@
+(ns mqtt
+  (:gen-class)
+  (:require [clojurewerkz.machine-head.client :as mh]
+            [compojure.core    :refer [defroutes GET]]
+            [compojure.handler :refer [site]]
+            [compojure.route   :as route]
+            [ring.adapter.jetty :as jetty]
+            [environ.core      :refer [env]]))
+
+(defroutes app
+           (GET "*" []
+                (let [id   "Clojure"
+                      conn (mh/connect "tcp://127.0.0.1:1883" id)]
+                  (mh/subscribe conn ["hello"] (fn [^String topic _ ^bytes payload]
+                                                 (println (String. payload "UTF-8"))))
+                  (mh/publish conn "hello" "Hello, world")
+                  {:status 200
+                   :headers {"Content-Type" "text/plain"}
+                   :body "Published message!"})))
+
+(defn -main
+  [& args]
+  (let [id   "Clojure"
+        conn (mh/connect "tcp://127.0.0.1:1883" id)]
+    (mh/subscribe conn ["hello"] (fn [^String topic _ ^bytes payload]
+                                   (println (String. payload "UTF-8"))))
+    (mh/publish conn "hello" "Hello, world")))")))
