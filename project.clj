9b524e3b0d17b5454c480c134b921e35a09f9426
E. Kevin Hall <hall@absinthe.org>
2015-12-11 16:22:41 -0500

    update

--- project.clj
+++ project.clj
@@ -1,10 +1,15 @@
 (defproject mqtt "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clojurewerkz/machine_head "1.0.0-beta8"]
                  [compojure "1.1.8"]
                  [ring/ring-jetty-adapter "1.2.2"]
-                 [environ "0.5.0"]])
+                 [environ "0.5.0"]]
+  :plugins [[environ/environ.lein "0.2.1"]]
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "queuing-standalone.jar"
+  :profiles {:production {:env {:production true}}})
